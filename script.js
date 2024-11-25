af9271aed820837050bfe5ac43072e68247d612a
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2024-11-25 19:54:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,30 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 
-  //your code here
+  const romanNumerals = Object.values(obj);
+  let result = "";
+
+  for (let i = 0; i < romanNumerals.length; i++) {
+    const [symbol, value] = romanNumerals[i];
+
+    // Handle subtraction cases (e.g., 900 -> CM, 90 -> XC)
+    if (i + 2 < romanNumerals.length) {
+      const [nextSymbol, nextValue] = romanNumerals[i + 2];
+      if (num >= value - nextValue) {
+        result += nextSymbol + symbol;
+        num -= value - nextValue;
+        continue;
+      }
+    }
+
+    // Add symbols for the current value
+    while (num >= value) {
+      result += symbol;
+      num -= value;
+    }
+  }
+
+  return result;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
